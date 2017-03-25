a266cd7c7255a9d9a31e4c67f89a14d34b3f73b9
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-03-26 03:25:20 +0700

    People

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - [Suave ⌁ 01-Route](Suave/01-Route)
 - [Suave ⌁ 02-Async](Suave/02-Async)
 - [Suave ⌁ 03-Restful](Suave/03-Restful)
+- [Suave ⌁ BuldingRestApi](Suave/BuldingRestApi)
---
+++ Suave/BuldingRestApi/Main.fsx
@@ -0,0 +1,53 @@
+#r "../../packages/Suave/lib/net40/Suave.dll"
+#r "../../packages/Newtonsoft.Json/lib/net40/Newtonsoft.Json.dll"
+
+open Suave
+// http://blog.tamizhvendan.in/blog/2015/06/11/building-rest-api-in-fsharp-using-suave/
+
+open Suave.Successful
+open Suave.Writers
+open Suave.Operators
+open Suave.Successful
+open Suave.Filters
+
+open System.Collections.Generic
+
+open Newtonsoft.Json
+open Newtonsoft.Json.Serialization
+
+[<AutoOpen>]
+module Db = 
+    type Person = {
+        Id: int
+        Name: string
+        Age: int
+        Email: string
+    }
+
+    let private peopleStorage = Dictionary<string, Person>()
+    let getPeople() = peopleStorage.Values  |> Seq.map id
+
+[<AutoOpen>]
+module Restful =
+    type 'a RestResource = {
+        GetAll : unit -> 'a seq
+    }
+
+    let JSON v =
+        let jsonSerializeSettings = JsonSerializerSettings()
+        jsonSerializeSettings.ContractResolver <- CamelCasePropertyNamesContractResolver()
+
+        JsonConvert.SerializeObject(v, jsonSerializeSettings) 
+        |> OK >=> setMimeType "application/json; charset=utf-8"
+
+    let rest resourceName resource =
+        let resourcePath = "/" + resourceName
+        let getAll = resource.GetAll() |> JSON
+        path resourcePath >=> GET >=> getAll
+
+
+let personWebPart = rest "people" {
+    GetAll = Db.getPeople
+}
+
+startWebServer defaultConfig personWebPart 
